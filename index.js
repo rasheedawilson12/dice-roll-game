98518208c10d2bcb64404518275cab4390139582
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-01-04 13:49:40 -0600

    file set up, button added, event listener added and working

---
+++ index.js
@@ -0,0 +1,36 @@
+function rollDice() {
+  //Dice 1
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+  var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+  var randomImageSource = "images/" + randomDiceImage;
+
+  var image1 = document.querySelectorAll("img")[0];
+
+  image1.setAttribute("src", randomImageSource);
+
+  //Dice 2
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var randomDiceImage = "dice" + randomNumber2 + ".png";
+
+  var randomImageSource = "images/" + randomDiceImage;
+
+  var image2 = document.querySelectorAll("img")[1];
+
+  image2.setAttribute("src", randomImageSource);
+
+  //Determine Winner
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = " Player 2 Wins 🚩";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw! 🎲";
+  }
+}
+
+var button = document.querySelector(".button");
+
+button.addEventListener("click", rollDice);
